132d3f983d2f879d3d66c661a027f0a466799e99
abhinavkumarwat004 <beenakumawat866866@gmail.com>
2025-02-27 11:57:52 +0530

    git configuration

---
+++ Machinelearning_1/ML_1.py
@@ -0,0 +1,49 @@
+#Arithmetic operation 
+# a = 2
+# b = 3
+# print( a + b )
+# print (a - b)
+# print (a * b)
+# print (a / b)
+ 
+#  Comparison operator
+# print (a == b )
+# print (a != b )
+# print (a > b )
+# print (a < b )
+# print (a >= b )
+# print (a <= b )
+
+
+# age = 16 
+# if (age >= 18):
+#     print ("eligible")
+# else:
+#     print("not[]")
+# marks = int (input("Enter your marks: "))
+# if(marks >= 90):
+#     print  ("grade A")
+# elif (90 > marks >= 80):
+#     print ("Grade B")
+# elif ( 80 > marks >= 70):
+#     print ("Grade C")    
+# elif( 70 > marks):
+#     print("Grade D")
+# TRY 2
+# marks = int (input("Enter your marks: "))
+# if(marks >= 90):
+#       Grade = "A"
+# elif (90 > marks >= 80):
+#      Grade = "B"
+# elif ( 80 > marks >= 70):
+#      Grade = "C"    
+# elif( 70 > marks):
+#      Grade = "D"
+# print ("Your grade is - ", Grade )
+# b = {"name": "Abhinav", "age": "20"}
+# for n in b.values():
+#     print(n)
+count = 1
+while count <=6:
+    print (count)
+    count +=1
